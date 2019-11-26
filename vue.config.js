9df230c2943bad84b55216354dc777cc3ebce805
patrick.lake2 <52745289+patricklake2@users.noreply.github.com>
2019-11-26 14:58:26 +0000

    modifyed deployment config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
-  publicPath: '/leeds-flight-emissions/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/leeds-flight-emissions/'
+    : '/'
 };
