ffff7fc1898bf239a963689d64fb57f86f7e8026
patrick.lake2 <52745289+patricklake2@users.noreply.github.com>
2019-11-27 16:46:22 +0000

    Added method to calculate quartiles

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <template>
   <v-app id="inspire">
-    <nav-bar :items="routes" title="Leeds' Flight Emissions" />
+    <nav-bar :items="routes" title="Leeds Bradford Flight Emissions" />
     <v-content>
       <v-container fluid>
         <div class="container">
-          <p v-if="errorMessage" style="color: red; font-weight:bold;">
-            {{ errorMessage }}
-          </p>
-          <router-view :items="flights" />
+          <p v-if="errorMessage" style="color: red; font-weight:bold;">{{ errorMessage }}</p>
+          <router-view :items="flights" :limits="quartiles" />
         </div>
       </v-container>
     </v-content>
     <v-footer app>
-      <span
-        >&copy; Patrick Lake,
-        <a href="https://odileeds.org">ODI Leeds</a> 2019</span
-      >
+      <span>
+        &copy; Patrick Lake,
+        <a href="https://odileeds.org">ODI Leeds</a> 2019
+      </span>
     </v-footer>
   </v-app>
 </template>
@@ -42,6 +40,32 @@ export default {
   computed: {
     routes() {
       return this.$router.options.routes;
+    },
+    quartiles() {
+      var data = [];
+      for (let flight of this.flights) {
+        data.push(flight["Emissions"]);
+      }
+      data.sort(function(a, b){return a-b});
+      let q1 = this.percentile(data, 0.33);
+      let q2 = this.percentile(data, 0.75);
+      return [q1, q2]
+    }
+  },
+  methods: {
+    percentile(arr, p) {
+      if (arr.length === 0) return 0;
+      if (typeof p !== "number") throw new TypeError("p must be a number");
+      if (p <= 0) return arr[0];
+      if (p >= 1) return arr[arr.length - 1];
+
+      var index = arr.length * p,
+        lower = Math.floor(index),
+        upper = lower + 1,
+        weight = index % 1;
+
+      if (upper >= arr.length) return arr[lower];
+      return arr[lower] * (1 - weight) + arr[upper] * weight;
     }
   },
   created() {
