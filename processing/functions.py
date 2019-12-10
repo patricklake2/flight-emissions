17f3ade015513e41acd13b741a59321bbd4a8286
patrick.lake2 <52745289+patricklake2@users.noreply.github.com>
2019-12-10 10:33:33 +0000

    Update functions.py

--- processing/functions.py
+++ processing/functions.py
@@ -48,57 +48,42 @@ def get_flight_type(row):
     else:
         return 'LongHaul'
 
-#make a request to FlightAware's API using the flight number, then try to match it to an aircraft in my emission factors table.
 def get_aircraft_inf(row, aircraft_df, api_creds):
-    url = "http://flightxml.flightaware.com/json/FlightXML2/FlightInfoEx?howMany=1&ident="
     average_factors = {'Domestic' : 11.1, 'ShortHaul' : 11.8, 'LongHaul' : 26.9}
-
-    flight_id = row.FlightNumber
-    request_url = f'{url}{flight_id}'
-    basic_auth_creds = requests.auth.HTTPBasicAuth(api_creds['User'], api_creds['Key'])
-    api_response = requests.get(request_url, auth=basic_auth_creds)
-
-    #default values (if data can't be found)
+    
+    #default values to use if API request doesn't work or credentials not specified
     code, name = '', ''
     factor = average_factors[row.Flight_Type]
 
-    if api_response:
-        results_json = api_response.json()
-        try:
-            code = results_json['FlightInfoExResult']['flights'][0]['aircrafttype']
-        except KeyError: 
-            pass    #if no results we'll keep the default values specified above
-    
-        if not code == '':
-            matches = aircraft_df[aircraft_df['Aircraft_Code'] == code]
-            number_matches = len(matches)
+    if api_creds:
+        url = "http://flightxml.flightaware.com/json/FlightXML2/FlightInfoEx?howMany=1&ident="
+        flight_id = row.FlightNumber
+        request_url = f'{url}{flight_id}'
+        basic_auth_creds = requests.auth.HTTPBasicAuth(api_creds['User'], api_creds['Key'])
+        api_response = requests.get(request_url, auth=basic_auth_creds)
 
-            if number_matches == 1:
-                factor = matches['Emissions_factor'].values[0]
-                name = matches.Plane.values[0]
+        code, name = '', ''
+        factor = average_factors[row.Flight_Type]
 
-            elif number_matches > 1:
-                newMatches = matches[matches['Type'] == row.Flight_Type]
-                factor = newMatches['Emissions_factor'].values[0]
-                name = newMatches.Plane.values[0]
+        if api_response:
+            results_json = api_response.json()
+            try:
+                code = results_json['FlightInfoExResult']['flights'][0]['aircrafttype']
+            except KeyError: 
+                pass    #if no results we'll keep the default values specified above
+        
+            if not code == '':
+                matches = aircraft_df[aircraft_df['Aircraft_Code'] == code]
+                number_matches = len(matches)
+
+                if number_matches == 1:
+                    factor = matches['Emissions_factor'].values[0]
+                    name = matches.Plane.values[0]
+
+                elif number_matches > 1:
+                    newMatches = matches[matches['Type'] == row.Flight_Type]
+                    factor = newMatches['Emissions_factor'].values[0]
+                    name = newMatches.Plane.values[0]
 
     return code, name, factor
 
-#Pass a config object containing properties 'user', 'key', 'email', 'repo'
-def github_commit(data, commit_msg, repo, path, user, email, key):
-    encoded_bytes = base64.b64encode(data.encode("utf-8"))
-    encoded_str = str(encoded_bytes, "utf-8")
-    url = f'https://api.github.com/repos/{user}/{repo}/contents/{path}'
-    credentials = requests.auth.HTTPBasicAuth(user, key)
-    headers = {
-        "message": commit_msg,
-        "committer": {
-            "name": user,
-            "email": email
-        },
-        "content": encoded_str,
-        "branch": "master"
-    }
-    headers_str = json.dumps(headers)
-    r = requests.put(url, auth=credentials, data=headers_str)
-    print(r.text)
